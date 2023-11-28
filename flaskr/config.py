242a2abe948114252e6d9f8433dd892d0b7d7af9
Andrea Diamantini <adjam@protonmail.com>
2023-11-28 10:28:07 +0100

    flaskr blog

---
+++ flaskr/config.py
@@ -0,0 +1,10 @@
+
+# Google config data
+GOOGLE_CLIENT_ID = "688745057781-96ua1aaa98r6c9jgq29pmntqh13hrtj2.apps.googleusercontent.com"
+GOOGLE_PROJECT_ID = "kinetic-scheme-405317"
+GOOGLE_AUTH_URI = "https://accounts.google.com/o/oauth2/auth"
+GOOGLE_TOKEN_URI = "https://oauth2.googleapis.com/token"
+GOOGLE_AUTH_PROVIDER_X509_CERT_URL = "https://www.googleapis.com/oauth2/v1/certs"
+GOOGLE_CLIENT_SECRET = "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"
+GOOGLE_REDIRECT_URIS = ("http://127.0.0.1:5000/login/callback")
+GOOGLE_JAVASCRIPT_ORIGINS = ("http://127.0.0.1:5000")
